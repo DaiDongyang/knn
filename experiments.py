55e1e2290f094bbd1c3aabe52a4b42fa11e76220
daidongyang <daidongyang12@gmail.com>
2017-11-03 23:26:25 +0800

    fix some bug of experiments.py

--- experiments.py
+++ experiments.py
@@ -1,5 +1,6 @@
 import knn
 import time
+import gc
 import numpy as np
 
 if __name__ == '__main__':
@@ -9,12 +10,12 @@ if __name__ == '__main__':
     result_file = open('result.txt', 'w')
     train_samples, train_ls = knn.load_sample_set(train_dir)
     test_samples, ground_ls = knn.load_sample_set(test_dir)
-    ks = [3, 5]
+    ks = [3, 5, 7, 9]
     get_knn_funcs = [knn.get_knn_e_dist, knn.get_knn_e_dist_with_kdtree, knn.get_knn_m_dist]
     get_knn_funcs_str = ['get_knn_e_dist', 'get_knn_e_dist_with_kdtree', 'get_knn_m_dist']
     get_label_funcs = [knn.get_label_by_knn, knn.get_label_by_wknn]
     get_label_funcs_str = ['get_label_by_knn', 'get_label_by_wknn']
-    pca_parameters = [4]
+    pca_parameters = [0, 4, 8, 16, 32, 64, 128]
     results = [
         ['k', 'get_knn_function', 'get_label_function', 'pca_parameters', 'accuracy', 'macro_precision', 'macro_recall',
          'macro_F1', 'execution_time', 'd']]
@@ -46,7 +47,7 @@ if __name__ == '__main__':
                     accuracy, precision, recall, F_1, macro_precision, macro_recall, macro_F_1 = knn.result_evaluate(
                         ground_labels, result_labels)
                     print('accuracy =', accuracy, file=result_file)
-                    # single_result.append(accuracy)
+                    single_result.append(accuracy)
                     print('precision =', precision, file=result_file)
                     # single_result.append(precision)
                     print('recall =', recall, file=result_file)
@@ -64,12 +65,14 @@ if __name__ == '__main__':
                     single_result.append(d)
                     print(file=result_file)
                     results.append(single_result)
-    result_file.close()
+                    gc.collect()
     experiment_end_t = time.time()
     print('***********************', file=result_file)
     print('Total Time =', experiment_end_t - experiment_start_t, file=result_file)
     print('***********************', file=result_file)
+    result_file.close()
     with open('result_table.csv', 'w') as outf:
         for ele in results:
-            ele_str = ','.join(ele)
+            str_arrays = [str(i) for i in ele]
+            ele_str = ', '.join(str_arrays)
             print(ele_str, file=outf)
